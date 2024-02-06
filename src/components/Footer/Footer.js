6126c7a2c745962eb17406686f5165dea0a24ec3
MD ASHIF REZA <55871288+mdashifreza@users.noreply.github.com>
2024-02-06 11:56:41 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white py-8">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
-                <div className="mb-4 md:mb-0">
+                // <div className="mb-4 md:mb-0">
                     // <h3 className="text-xl font-bold">Subscribe our newsletter</h3>
                     // <p className="text-sm">No ज्ञान, no spam, only मुद्दे की बात।</p>
-                </div>
+                // </div>
                 <div className="mb-4 md:mb-0">
                     <label htmlFor="email" className="text-sm block">Email</label>
                     <input
